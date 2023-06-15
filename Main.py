3452d15821896d2edd07afb13e9cc9809929423c
bhathiya <bhathiyabandara87@gmail.com>
2023-06-15 22:17:47 +0530

    Final Commit -added header,total price, company name

--- Main.py
+++ Main.py
@@ -20,11 +20,12 @@ for filePath in filePaths:
     pdf.cell(w=50, h=8, txt=f"Invoice No {invoiceNo}", ln=1)  # writing the invoice no into the pdf
 
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f"Date {date}",ln=1)  # writing the Date into the pdf
+    pdf.cell(w=50, h=8, txt=f"Date {date}", ln=1)  # writing the Date into the pdf
 
     # Reading the excel files and iterating the rows
     df = pd.read_excel(filePath, sheet_name='Sheet 1')
-    columns = list(df.columns)  # getting the column headers of the excel file using df.columns method and putting it to a list
+    columns = list(
+        df.columns)  # getting the column headers of the excel file using df.columns method and putting it to a list
     columns = [item.replace('_', ' ').title() for item in columns]  # replacing the _ and capitalizing the first letter
     # printing the column headers
     pdf.set_font(family='Times', size=10, style='B')
@@ -35,6 +36,7 @@ for filePath in filePaths:
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
+    total = 0
     # Adding rows to the pdf
     for index, row in df.iterrows():
         pdf.set_font(family='Times', size=10)
@@ -44,5 +46,24 @@ for filePath in filePaths:
         pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1,ln=1)
+        # Incrementing the total according to each iterations total price
+        total += row['total_price']
+
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total), border=1, ln=1)
+
+    # Adding the line to display the total
+    pdf.set_font(family='Times', size=10)
+    pdf.cell(w=30, h=8, txt=f" The total price is {total}", ln=1)
+
+    # Adding the company name and logo
+    pdf.set_font(family='Times', size=10)
+    pdf.cell(w=30, h=8, txt=f" Company Name is ABCD INC", ln=1)
+
 
     pdf.output(f"PDFs/{fileName}.pdf")
