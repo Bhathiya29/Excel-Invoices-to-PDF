17793a8503e5e7befb1d17fdbb4a5d3f2147ebc2
bhathiya <bhathiyabandara87@gmail.com>
2023-06-15 20:27:17 +0530

    load the excel file into dataframes

--- Main.py
+++ Main.py
@@ -1,2 +1,9 @@
 # APP TO GENERATE PDF INVOICES FROM EXCEL FILES
+import pandas as pd
+import glob
 
+filePaths = glob.glob('Invoices/*.xlsx')  # Getting all the exel files into a list
+
+for filePath in filePaths:
+    df = pd.read_excel(filePath,sheet_name='Sheet 1')
+    print(df)
